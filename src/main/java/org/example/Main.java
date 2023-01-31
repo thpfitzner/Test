3d4a826795cf9c08ee56c004fc47e4be11a0e693
thpfitzner <zorgus@web.de>
2023-01-31 09:26:52 +0100

    enum

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,13 @@
 package org.example;
 
+import org.example.enums.Company;
+
 public class Main {
     public static void main(String[] args) {
+        String name = Company.EBAY.name();
+        int ordinal = Company.EBAY.ordinal();
+        Company ebay = Company.valueOf("EBAY");
+        Company[] values = Company.values();
         System.out.println("Hello world!");
     }
 }
---
+++ src/main/java/org/example/enums/Company.java
@@ -0,0 +1,15 @@
+package org.example.enums;
+
+public enum Company {
+    EBAY(30), PAYPAL(10), GOOGLE(15), YAHOO(20), ATT(25);
+
+    private int value;
+
+    private Company(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
